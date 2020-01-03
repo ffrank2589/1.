48975a555ce7614d202a59094eab53e3a4d8be8c
ffrank2589 <ffrank2589@yahoo.com.tw>
2020-01-03 13:55:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ON1.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class ON1 {
+    public static void main(String[] args) {
+        System.out.println();
+        for (int i = 65; i < 91; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+        for (int i = 97; i < 123; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+    }
+}
